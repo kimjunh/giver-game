ee88e90ef3ec18c4164bd71e45722febbc3b3ee9
Junhyung Kim <kimjunh4u@gmail.com>
2016-04-01 04:19:29 +0000

    coverage hates me

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
 
-#require 'simplecov'
+# require 'simplecov'
+
+require "codeclimate-test-reporter"
+# CodeClimate::TestReporter.start
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
