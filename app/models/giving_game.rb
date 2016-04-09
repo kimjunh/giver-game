c24973ede4364de83d7edba5357f69a9e0fd5f8f
Nicholas Choi <nick.k.choi@berkeley.edu>
2016-04-09 22:43:23 +0000

    added some validations and model rspec tests

--- app/models/giving_game.rb
+++ app/models/giving_game.rb
@@ -1,5 +1,12 @@
 class GivingGame < ActiveRecord::Base
   belongs_to :user
+
+  # needs titles for all of the titles of things.
+  validates_presence_of :title, :charityA_title, :charityB_title, :total_money, :per_transaction
+
+  #validates that the moneys need to be numebrs
+  validates_numericality_of :per_transaction, :total_money
+
   def voteForA
     self.votesA += 1
     self.save
--- spec/models/giving_game_spec.rb
+++ spec/models/giving_game_spec.rb
@@ -16,4 +16,32 @@ RSpec.describe GivingGame, :type => :model do
             givingGame.votesB.should == 1
         end        
     end
+    
+    describe 'creation of a game' do
+        it 'should allow you to create giving games' do
+            givingParams = {:title => "title", :description => "description", 
+            :total_money => 10.00, :per_transaction => 1.00, 
+            :charityA_title => "charity A", :descriptionA => "description A", 
+            :charityB_title => "charity B", :descriptionB => "description B", 
+            :tutorial => false}
+            givingGame = GivingGame.create(givingParams)
+            givingGame.valid?.should be true
+        end
+        
+        it 'should require the presence of the necessary fields' do 
+            givingGame = GivingGame.create({:title => nil, :description => nil, 
+            :total_money => nil, :per_transaction => nil, 
+            :charityA_title => nil, :descriptionA => nil, 
+            :charityB_title => nil, :descriptionB => nil,})
+            givingGame.valid?.should be false
+            print givingGame.errors.messages
+            print "\n"
+        end
+        
+        it 'should require that the Total amount and the per transaction be numbers' do 
+            givingGame = GivingGame.create({:total_money => 'aafb', :per_transaction => '*&3'})
+            givingGame.valid?.should be false
+            print givingGame.errors.messages
+        end
+    end
 end
